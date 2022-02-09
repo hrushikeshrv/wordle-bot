ef7aede96a7da345dcb09685790b3dd9cbba2003
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2022-02-09 09:43:04 +0530

    v0.1.0 alpha done

--- simulator.py
+++ simulator.py
@@ -15,11 +15,17 @@ class Simulator:
         assert len(self.fixed_characters) <= self.length
     
     def add_containing_chars(self, chars):
+        if chars == '_':
+            return
         self.containing_characters += chars
+        self.containing_characters = ''.join(set(self.containing_characters))
         assert len(self.containing_characters) <= self.length
     
     def exclude_chars(self, chars):
+        if chars == '_':
+            return
         self.exclude_characters += chars
+        self.exclude_characters = ''.join(set(self.exclude_characters))
     
     def get_possible_guesses(self):
         return generate_words(
@@ -52,7 +58,8 @@ class Simulator:
         for i in range(6):      # We have a total of 6 guesses
             response = input('\n---------\nEnter the response to your last guess - ').strip()
             if response == 'W':
-                return 'Great!'
+                print('Great!')
+                return
             # The response will have format "fixed_chars<SPACE>containing_chars<SPACE>exclude_chars"
             _ = response.split()
             response_dict = {
@@ -61,6 +68,6 @@ class Simulator:
                 'exclude_characters': _[2],
             }
             self.parse_response(response_dict)
-            print('Generating your next guesses -\n')
+            print('\nGenerating your next guesses...')
             words = self.get_possible_guesses()
-            print(f'Your next guesses could be one of the following - \n{words[:10]}')
+            print(f'Guess one of the following words next - \n{words[:10]}')
